3c7f413e8b13f8b43558a0025d3167f60adaf20a
David Walsh <>
2023-10-25 23:09:28 +0100

    Added basic player inputs and movement

---
+++ characters/character_mover.gd
@@ -0,0 +1,41 @@
+extends Node3D
+
+@export var jump_force = 15.0
+@export var gravity = 30.0
+
+@export var max_speed = 15.0
+@export var move_accel = 4.0
+@export var stop_drag = 0.9
+
+var character_body : CharacterBody3D
+var move_drag = 0.0
+var move_dir : Vector3
+
+func _ready():
+	character_body = get_parent()
+	move_drag = float(move_accel) / max_speed
+
+func set_move_dir(new_move_dir : Vector3):
+	move_dir = new_move_dir
+
+func jump():
+	if character_body.is_on_floor():
+		character_body.velocity.y = jump_force
+
+# Runs 60 times a sec, fixedupdate in Unity
+func _physics_process(delta):
+	if character_body.velocity.y > 0.0 and character_body.is_on_ceiling():
+		character_body.velocity.y = 0.0
+	if not character_body.is_on_floor():
+		character_body.velocity.y -= gravity * delta
+	
+	var drag = move_drag
+	if move_dir.is_zero_approx():
+		drag = stop_drag
+	
+	var flat_velo = character_body.velocity
+	flat_velo.y = 0.0
+	character_body.velocity += move_accel * move_dir - flat_velo * drag
+	
+	character_body.move_and_slide()
+	
---
+++ characters/player/player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody3D
+
+@onready var camera_3d = $Camera3D # The $Camera3D is equivalent to get_node("Camera3D")
+@onready var character_mover = $CharacterMover
+
+@export var mouse_sensitivity_h = 0.15 # Horizontal mouse sens
+@export var mouse_sensitivity_v = 0.15 # Vertical mouse sens
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= event.relative.x * mouse_sensitivity_h # Horizontal mouse look
+		camera_3d.rotation_degrees.x -= event.relative.y * mouse_sensitivity_v # Vertical mouse look
+		camera_3d.rotation_degrees.x = clamp(camera_3d.rotation_degrees.x, -90, 90) # Clamps mouse look to not be able to slip
+
+# Equivalent to update() in Unity, runs every frame
+func _process(delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("restart"):
+		get_tree().reload_current_scene()
+	if Input.is_action_just_pressed("fullscreen"):
+		var fs = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
+		if fs:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
+	var move_dir = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	character_mover.set_move_dir(move_dir)
+	if Input.is_action_just_pressed("jump"):
+		character_mover.jump()
