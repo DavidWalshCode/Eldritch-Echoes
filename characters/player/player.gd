57ac8cb207bd425204ef81de390802435e751b5e
David Walsh <20276885@studentmail.ul.ie>
2023-11-28 13:24:56 +0000

    Updating structure of the game files and assets to be cleaner and clearer

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -3,8 +3,8 @@ extends CharacterBody3D
 @onready var camera_3d = $Camera3D # The $Camera3D is equivalent to get_node("Camera3D")
 @onready var character_mover = $CharacterMover
 
-@export var mouse_sensitivity_h = 0.15 # Horizontal mouse sens
-@export var mouse_sensitivity_v = 0.15 # Vertical mouse sens
+@export var mouse_sensitivity_h = 0.13 # Horizontal mouse sens
+@export var mouse_sensitivity_v = 0.13 # Vertical mouse sens
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -17,20 +17,20 @@ func _input(event):
 
 # Equivalent to update() in Unity, runs every frame
 func _process(delta):
-	if Input.is_action_just_pressed("quit"):
+	if Input.is_action_just_pressed("quit"): # Currently 'Esc'
 		get_tree().quit()
-	if Input.is_action_just_pressed("restart"):
+	if Input.is_action_just_pressed("restart"): # Currently 'r'
 		get_tree().reload_current_scene()
-	if Input.is_action_just_pressed("fullscreen"):
+	if Input.is_action_just_pressed("fullscreen"): # Currently 'f'
 		var fs = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
 		if fs:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards") # Currently 'WASD'
 	var move_dir = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	character_mover.set_move_dir(move_dir)
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump"): # Currently 'Space'
 		character_mover.jump()
