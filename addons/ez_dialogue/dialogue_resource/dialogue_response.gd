557cc0fd9935b5c0cedf0f444f1e67cee2435e12
David Walsh <20276885@studentmail.ul.ie>
2024-01-14 03:49:10 +0000

    Adding new dialogue manager

---
+++ addons/ez_dialogue/dialogue_resource/dialogue_response.gd
@@ -0,0 +1,16 @@
+class_name DialogueResponse extends Resource
+
+var text: String = ""
+var choices: Array[String] = []
+
+func append_text(_text: String):
+	if (!text.is_empty()):
+		text += "\n" + _text
+	else:
+		text += _text
+
+func append_choice(_choice: String):
+	choices.push_back(_choice)
+	
+func is_empty():
+	return text.length() == 0 && choices.size() == 0
