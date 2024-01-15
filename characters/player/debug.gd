d71f988fe6b83ee9529a8b0d5d38f619e0ed1c52
David Walsh <20276885@studentmail.ul.ie>
2024-01-15 02:03:20 +0000

    Adding debug ui

---
+++ characters/player/debug.gd
@@ -0,0 +1,30 @@
+extends PanelContainer
+
+@onready var property_container = $MarginContainer/VBoxContainer
+var frames_per_second : String
+
+func _ready():
+	Global.debug = self # Set global reference to self in Global Singleton
+	visible = false
+
+func _process(delta):
+	if visible:
+		frames_per_second = "%.2f" % (1.0 / delta)
+		add_property("FPS", frames_per_second, 1)
+
+func _input(event):
+	if event.is_action_pressed("debug"):
+		visible = !visible
+
+# Debug function to add and update property
+func add_property(title : String, value, order):
+	var target
+	target = property_container.find_child(title, true, false) # Try to find Label node with same node
+	if !target: # If there is no current Label node for property (i.e. initial load)
+		target = Label.new() # Create new Label node
+		property_container.add_child(target) # Add new node as child to VBox container
+		target.name = title # Set name to title
+		target.text = target.name + ": " + str(value) # Set text value
+	elif visible:
+		target.text = title + ": " + str(value) # Update text value
+		property_container.move_child(target, order) # Reorder property based on given order value
--- global.gd
+++ global.gd
@@ -1,4 +1,4 @@
 extends Node
 
-# Global script for keeping track of the player death count
-var death_count = 0
+var death_count = 0 # Keeping track of the player death count
+var debug # Reference to DebugPanel for debug property assignment
